3aa11c76b7df07ad25e2652b1dab861fb3093f9d
George Fekete <george@primalskill.com>
2014-06-24 09:22:16 +0300

    Fixes and comments

--- Vagrantfile
+++ Vagrantfile
@@ -6,13 +6,16 @@ Vagrant.configure("2") do |config|
 	# Setup port forwarding
 	config.vm.network :forwarded_port, guest: 80, host: 8931, auto_correct: true
 
+    # Setup synced folder
     config.vm.synced_folder "./", "/var/www", create: true, group: "www-data", owner: "www-data"
 
+    # VM specific configs
     config.vm.provider "virtualbox" do |v|
     	v.name = "SitePoint Test Vagrant"
     	v.customize ["modifyvm", :id, "--memory", "1024"]
     end
 
+    # Shell provisioning
     config.vm.provision "shell" do |s|
     	s.path = "provision/setup.sh"
     end
--- provision/setup.sh
+++ provision/setup.sh
@@ -2,35 +2,43 @@
 
 echo "Provisioning virtual machine..."
 
+# Git
 echo "Installing Git"
-#apt-get install git -y > /dev/null
+apt-get install git -y > /dev/null
 
+# Nginx
 echo "Installing Nginx"
-#apt-get install nginx -y > /dev/null
+apt-get install nginx -y > /dev/null
 
+# PHP
 echo "Updating PHP repository"
-#apt-get install python-software-properties build-essential -y > /dev/null
-#add-apt-repository ppa:ondrej/php5 -y > /dev/null
-#apt-get update > /dev/null
+apt-get install python-software-properties build-essential -y > /dev/null
+add-apt-repository ppa:ondrej/php5 -y > /dev/null
+apt-get update > /dev/null
 
 echo "Installing PHP"
-#apt-get install php5-common php5-dev php5-cli php5-fpm -y > /dev/null
+apt-get install php5-common php5-dev php5-cli php5-fpm -y > /dev/null
 
 echo "Installing PHP extensions"
-#apt-get install curl php5-curl php5-gd php5-mcrypt php5-mysql -y > /dev/null
+apt-get install curl php5-curl php5-gd php5-mcrypt php5-mysql -y > /dev/null
 
+# MySQL 
 echo "Preparing MySQL"
-#apt-get install debconf-utils -y > /dev/null
-#debconf-set-selections <<< "mysql-server mysql-server/root_password password 1234"
-#debconf-set-selections <<< "mysql-server mysql-server/root_password_again password 1234"
+apt-get install debconf-utils -y > /dev/null
+debconf-set-selections <<< "mysql-server mysql-server/root_password password 1234"
+debconf-set-selections <<< "mysql-server mysql-server/root_password_again password 1234"
 
 echo "Installing MySQL"
-#apt-get install mysql-server -y > /dev/null
+apt-get install mysql-server -y > /dev/null
 
+# Nginx Configuration
 echo "Configuring Nginx"
 cp /var/www/provision/config/nginx_vhost /etc/nginx/sites-available/nginx_vhost > /dev/null
 ln -s /etc/nginx/sites-available/nginx_vhost /etc/nginx/sites-enabled/
 
 rm -rf /etc/nginx/sites-available/default
 
+# Restart Nginx for the config to take effect
 service nginx restart > /dev/null
+
+echo "Finished provisioning."
